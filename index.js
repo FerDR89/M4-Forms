b1ef0340a1e8ad790d24a20e42d91327ed26b1d8
Fernando de Row <ferderow@gmail.com>
2021-06-05 12:05:21 -0300

    Agregado de lógica para capturar los datos del form con JS

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function main() {}
+const handleEvent = (event) => {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const value = Object.fromEntries(data.entries());
+  value.optionsCheckbox = data.getAll("optionsCheckbox");
+  console.log(event.target);
+};
+
+const formListener = () => {
+  const myForm = document.querySelector("form");
+  myForm.addEventListener("submit", handleEvent);
+};
+
+function main() {
+  formListener();
+}
 
 main();
